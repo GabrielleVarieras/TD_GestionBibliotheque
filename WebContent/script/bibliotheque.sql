7fac9ebaef2c4a18c7a807d9e346ad2745e2d207
HighSanda <50743512+HighSanda@users.noreply.github.com>
2019-05-23 16:48:22 +0200

    Add files via upload

---
+++ WebContent/script/bibliotheque.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 23 mai 2019 à 09:41
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bibliotheque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+
+DROP TABLE IF EXISTS `livre`;
+CREATE TABLE IF NOT EXISTS `livre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(60) DEFAULT NULL,
+  `auteur` varchar(60) DEFAULT NULL,
+  `prix` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `livre`
+--
+
+INSERT INTO `livre` (`id`, `titre`, `auteur`, `prix`) VALUES
+(1, 'La  vie', 'ny(', 52),
+(2, 'Lavie', 'Georges', 52);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
